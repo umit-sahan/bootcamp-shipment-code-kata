535db6356612c3838975e24ea811d0eb2214c980
umit-sahan <you.mit01@gmail.com>
2023-10-02 14:55:04 +0300

    [umit-sahan]CompleteProject

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,45 @@
 package com.trendyol.shipment;
 
 import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Basket {
-
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+        if (getProducts().isEmpty()) {
+            throw new IllegalStateException("Basket is empty.");
+        }
+
+        Map<ShipmentSize, Long> sizeCounts = getProducts().stream()
+                .collect(Collectors.groupingBy(Product::getSize, Collectors.counting()));
+
+        Optional<Map.Entry<ShipmentSize, Long>> maxEntry = sizeCounts.entrySet().stream()
+                .max(Map.Entry.comparingByValue());
+
+        ShipmentSize maxShipmentSize = maxEntry.map(Map.Entry::getKey)
+                .orElse(ShipmentSize.SMALL);
+
+        return maxEntry
+                .filter(entry -> entry.getValue() >= 3)
+                .map(entry -> determineSizeOneStepUp(maxShipmentSize))
+                .orElse(determineLargestShipmentSize());
+    }
+
+    private ShipmentSize determineLargestShipmentSize() {
+        return getProducts().stream()
+                .map(Product::getSize)
+                .max(Comparator.naturalOrder())
+                .orElse(ShipmentSize.SMALL);
+    }
+
+    private ShipmentSize determineSizeOneStepUp(ShipmentSize maxShipmentSize) {
+        return maxShipmentSize == ShipmentSize.SMALL
+                ? ShipmentSize.MEDIUM
+                : (maxShipmentSize == ShipmentSize.MEDIUM
+                ? ShipmentSize.LARGE
+                : ShipmentSize.X_LARGE);
     }
 
     public List<Product> getProducts() {
@@ -18,3 +50,4 @@ public class Basket {
         this.products = products;
     }
 }
+
